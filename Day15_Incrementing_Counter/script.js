c43b3b793c5301fc5af28a5cc9317ec1b147aafa
Chris Castellano <c.castellano18@gmail.com>
2021-08-23 17:00:09 -0500

    day15

---
+++ Day15_Incrementing_Counter/script.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.counters');
+
+counters.forEach((counter) => {
+  counter.innerText = '0';
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute('data-target');
+    const c = +counter.innerText;
+    console.log(target);
+    const increment = target / 200;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
